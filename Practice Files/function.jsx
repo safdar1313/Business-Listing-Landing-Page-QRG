983316482df099537710c7a1e984b4b25179f4eb
safdar1313 <safdarahmad003@gmail.com>
2024-09-19 08:37:36 +0100

    Add files via upload

---
+++ Practice Files/function.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Safdar (){
+    return(
+        <>
+            <h1>Safdar Ali Is</h1>
+            <p>Using React.js</p>
+        </>
+    )
+}
+export default Safdar;
